e7993dc4822be05c38a699b02fe4a96609c0f834
Iwan1214 <m-8400452@moe-dl.edu.my>
2026-04-28 12:33:31 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,22 @@
+const apiKey = "YOUR_API_KEY";
+const city = "Putrajaya";
+
+async function getWeather() {
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    document.getElementById("temp").innerText = data.main.temp + " °C";
+    document.getElementById("hum").innerText = data.main.humidity + " %";
+    document.getElementById("wind").innerText = data.wind.speed + " km/h";
+    document.getElementById("rain").innerText = data.weather[0].main;
+
+  } catch (error) {
+    console.log("Error loading weather:", error);
+  }
+}
+
+getWeather();
+setInterval(getWeather, 60000);

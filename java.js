8c9940e8d2e7097ab05065789fe6e97e562dcf68
Iwan1214 <m-8400452@moe-dl.edu.my>
2026-04-28 12:52:03 +0800

    Add files via upload

--- java.js
+++ java.js
@@ -1,22 +1,27 @@
 const apiKey = "6f49b0d372e46afd7003341110fe5c01";
-const city = "Putrajaya";
 
-async function getWeather() {
-  try {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-    const res = await fetch(url);
-    const data = await res.json();
-
-    document.getElementById("temp").innerText = data.main.temp + " °C";
-    document.getElementById("hum").innerText = data.main.humidity + " %";
-    document.getElementById("wind").innerText = data.wind.speed + " km/h";
-    document.getElementById("rain").innerText = data.weather[0].main;
-
-  } catch (error) {
-    console.log("Error loading weather:", error);
-  }
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(success, error);
 }
 
-getWeather();
-setInterval(getWeather, 60000);
+async function success(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+  const url =
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  document.getElementById("temp").innerText = data.main.temp + " °C";
+  document.getElementById("hum").innerText = data.main.humidity + " %";
+  document.getElementById("wind").innerText = data.wind.speed + " km/h";
+  document.getElementById("rain").innerText = data.weather[0].main;
+}
+
+function error() {
+  alert("Location access denied!");
+}
+
+getLocation();
